9f743e44fc174bb14b9919cf69a4e683a4aaab0c
Mudasir Mujtaba <mudasirmujtaba15@gmail.com>
2024-08-07 17:49:45 +0500

    layout changed

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from './Components/Header/Header.jsx';
+import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer.jsx';
+
 
 export default function Layout() {
   return (
     <>
       <Header />
+      <Outlet />
+      <Footer />
     </>
   );
 }
