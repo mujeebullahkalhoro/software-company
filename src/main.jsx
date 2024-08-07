e6823e5ef9ce757f5f60dcf50b29a129efef7d0b
Mudasir Mujtaba <mudasirmujtaba15@gmail.com>
2024-08-07 23:20:20 +0500

    all updated files

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React, { Children } from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import { createBrowserRouter } from 'react-router-dom';
